43c629af824365938b7a023a8189b04f13fcb71d
vonchoxx <stivenhernan1258@gmail.com>
2024-03-04 00:18:48 -0500

    1.0.55

--- ProyectoInventario/src/proyecto/espe/modelos/Cliente.java
+++ ProyectoInventario/src/proyecto/espe/modelos/Cliente.java
@@ -9,5 +9,96 @@ package proyecto.espe.modelos;
  * @author PC
  */
 public class Cliente {
-    
+
+  private String cedula, nombre, apellido, telefono,direccion;
+  private int idPerfil;
+  private String nombrePerfil;
+
+    public Cliente() {
+    }
+
+    public Cliente(String cedula, String nombre, String apellido, String telefono, String direccion) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public Cliente(String cedula, String nombre, String apellido, String telefono, String direccion, int idPerfil, String nombrePerfil) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.idPerfil = idPerfil;
+        this.nombrePerfil = nombrePerfil;
+    }
+
+    public Cliente(String cedula, String nombre, String apellido, String telefono, String direccion, int idPerfil) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.idPerfil = idPerfil;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getIdPerfil() {
+        return idPerfil;
+    }
+
+    public void setIdPerfil(int idPerfil) {
+        this.idPerfil = idPerfil;
+    }
+
+    public String getNombrePerfil() {
+        return nombrePerfil;
+    }
+
+    public void setNombrePerfil(String nombrePerfil) {
+        this.nombrePerfil = nombrePerfil;
+    }
+  
+  
 }
