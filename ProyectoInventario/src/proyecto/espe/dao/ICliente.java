469cfc164662aa79339c334a1721d23cc1ee7e79
vonchoxx <stivenhernan1258@gmail.com>
2024-03-04 00:19:52 -0500

    1.0.56

--- ProyectoInventario/src/proyecto/espe/dao/ICliente.java
+++ ProyectoInventario/src/proyecto/espe/dao/ICliente.java
@@ -4,10 +4,22 @@
  */
 package proyecto.espe.dao;
 
+import java.util.List;
+import proyecto.espe.modelos.Cliente;
+
 /**
  *
  * @author PC
  */
 public interface ICliente {
-    
+
+    public List<Cliente> listaCliente();
+
+    public Cliente BuscarCedulaCliente(String cedula);
+
+    public boolean InsertarCliente(Cliente cliente);
+
+    public boolean ActualizarCliente(Cliente cliente);
+
+    public boolean EliminarCliente(String cedula);
 }
